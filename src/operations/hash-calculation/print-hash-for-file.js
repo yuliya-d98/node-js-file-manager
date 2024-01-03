68690ccc236bca29a9e4f274d23ee09b6cc2356a
Yuliya Dubovik <feelgloom@gmail.com>
2024-01-03 22:53:47 +0300

    feat: hash method

--- src/operations/hash-calculation/print-hash-for-file.js
+++ src/operations/hash-calculation/print-hash-for-file.js
@@ -1,5 +1,26 @@
-const printHashForFile = (pathToFile) => {
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+import checkIfPathValid from "../../utils/check-if-path-valid.js";
 
+const printHashForFile = async (pathToFile) => {
+    await checkIfPathValid(pathToFile);
+
+    try {
+        const file = await readFile(pathToFile, { encoding: 'utf8' });
+
+        const hash = createHash('sha256');
+        hash.on('readable', () => {
+            const data = hash.read();
+            if (data) {
+                console.log(data.toString('hex'));
+            }
+        });
+
+        hash.write(file);
+        hash.end();
+    } catch (e) {
+
+    }
 }
 
 export default printHashForFile;
